91b5a654e9b0e05b1936b7871bc5205636327d77
william <luroylau@163.com>
2019-04-12 14:07:09 +0800

    StreamSubscription to listen ConnectivityResult; Initiate subscription in initState

--- lib/demos/http_demo.dart
+++ lib/demos/http_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:connectivity/connectivity.dart';
@@ -10,6 +12,25 @@ class HttpDemo extends StatefulWidget {
 class _HttpDemoState extends State<HttpDemo> {
   var _connectionStatus = 'unknown';
   Connectivity connectivity;
+
+  StreamSubscription<ConnectivityResult> subscription;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    connectivity = Connectivity();
+    subscription =
+        connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      _connectionStatus = result.toString();
+      print(_connectionStatus);
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        setState(() {});
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
