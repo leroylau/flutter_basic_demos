30f3a6d9f8fc60f33d63943706309e3751821d1a
william <luroylau@163.com>
2019-04-12 11:22:59 +0800

    Add http package

---
+++ lib/demos/http_demo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'http';
+
+class HttpDemo extends StatefulWidget {
+  @override
+  _HttpDemoState createState() => _HttpDemoState();
+}
+
+class _HttpDemoState extends State<HttpDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HttpDemo'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.yellow[700],
       appBar: AppBar(
         title: Text(widget.title),
       ),
