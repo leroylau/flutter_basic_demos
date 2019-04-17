9c5acc9d710ba03f952c65d9e40c575a06b40d0a
william <luroylau@163.com>
2019-04-17 10:52:45 +0800

    the member para and constrcutor

--- lib/demos/burn_off_flip_card/item_card.dart
+++ lib/demos/burn_off_flip_card/item_card.dart
@@ -1,6 +1,11 @@
+import 'package:basic_demo/models/burn_off_flip_card/aliment.dart';
 import 'package:flutter/material.dart';
 
 class ItemCard extends StatelessWidget {
+  final Aliment aliment;
+
+  const ItemCard({Key key, this.aliment}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container();
