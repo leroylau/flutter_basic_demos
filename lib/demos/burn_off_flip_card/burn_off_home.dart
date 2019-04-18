d9c27d102f0941fd9960e8cd50b0b5d79c92a979
william <luroylau@163.com>
2019-04-18 08:29:10 +0800

    fix a bug in fontFamily

--- lib/demos/burn_off_flip_card/burn_off_home.dart
+++ lib/demos/burn_off_flip_card/burn_off_home.dart
@@ -1,5 +1,4 @@
 import 'package:basic_demo/demos/burn_off_flip_card/item_card.dart';
-import 'package:basic_demo/models/burn_off_flip_card/aliment.dart';
 import 'package:basic_demo/models/burn_off_flip_card/aliments.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,13 +10,16 @@ class BurnOffHome extends StatelessWidget {
     );
   }
 
-  final aliment = Aliments.aliments[0];
-
   @override
   Widget build(BuildContext context) {
+    final aliment = Aliments.aliments[0];
+
     return Scaffold(
-      body: ItemCard(),
-      // TODO: the body of scffold is stacked with a series of widgets
+      body: Center(
+        child: ItemCard(
+          aliment: aliment,
+        ),
+      ),
     );
   }
 }
